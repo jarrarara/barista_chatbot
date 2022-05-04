333bfea9963ef7965a40bd6a3746f0590f73fffc
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-04 20:29:17 +1200

    Added Comments

--- barista_bot.py
+++ barista_bot.py
@@ -1,13 +1,54 @@
 #Jarra Piatos- Barista chatbot program: Digital Assessment Term 1
 
-import random #libary that is within python
-from random import randint #random intiger (random number generator)
+#Welcome message with random name
 
-#list of randomly generated names
-names = ["Zunalora", "Kyzumath", "Malos", "Manazyri", "Zycandos", "Cornelius", "Aquilla", "Lelaine", "Doriath", "Zephyr"]
 
-num = randint(0,9) #picks out random names to generate in the code
-name = (names[num])
 
-print(name) #prints the name thats is randomly generated
 
+
+#Menu for pickup or delivery
+
+
+
+
+#Pickup information - name and phone number
+
+
+
+
+#Delivery infromation - name, address and phone
+
+
+
+
+#Choose total number of drinks - maximum 5 drinks per customer
+
+
+
+
+#Drinks menu
+
+
+
+
+#Drinks order from menu - print each drink order with cost
+
+
+
+
+#Print order out - including if order is delivery or pickup - names and price of each drink - total cost including any delivery charge
+
+
+
+
+#Ability to cancel or proceed with the order
+
+
+
+
+#Option for new order or to exit
+
+
+
+
+#Main function
