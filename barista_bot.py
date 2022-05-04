f20e9b508a93a05ed06b2edb8f16a3b725c0c277
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-04 20:34:40 +1200

    updated version

--- barista_bot.py
+++ barista_bot.py
@@ -1,10 +1,22 @@
 #Jarra Piatos- Barista chatbot program: Digital Assessment Term 1
 
+from random import randint #random intiger (random number generator)
+
+#list of randomly generated names
+names = ["Zunalora", "Kyzumath", "Malos", "Manazyri", "Zycandos", "Cornelius", "Aquilla", "Lelaine", "Doriath", "Zephyr"]
+
 #Welcome message with random name
-
-
-
-
+def welcome(): #defines fuction welcome
+    '''
+    Purpose: to generate a random name from the list and print a welcome message to the user
+    Parameters: none
+    Returns: None
+    '''
+    num = randint(0,9) #picks out random names to generate in the code
+    name = (names[num])
+    print("o()xx[{:::::::::::::::: Welcome to Cafe Encantadia :::::::::::::>") #prints the welcome message along with the randomly generated name
+    print("✧─ ･ ｡ﾟ★: *.✦ .* :★. ─✧ My name is", name, "✧─ ･ ｡ﾟ★: *.✦ .* :★. ─✧")
+    print("I will be asissting you for today. How may I help our dear costumer? ( ✿ ◠ ‿ ◠ )")
 
 #Menu for pickup or delivery
 
@@ -52,3 +64,12 @@
 
 
 #Main function
+def main():
+    '''
+    Purpose: to run all functions
+    Parameters: none
+    Returns: None
+    '''
+    welcome()
+
+main()
