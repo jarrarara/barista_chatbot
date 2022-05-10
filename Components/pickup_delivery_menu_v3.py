690c27554fef3c0edbf50e96208073eee9803083
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-10 21:48:04 +1200

    version 4

---
+++ Components/pickup_delivery_menu_v3.py
@@ -0,0 +1,27 @@
+#user can choose whether they want pickup or delivery
+
+#bugs
+#need to make it so that it only accepts 1 or 2
+
+print ("Do you want your magical drink to be picked up or delivered? ")
+
+print ("For pickup, please enter '1'")
+print ("For delivery, please enter '2'")
+
+low = 1
+high = 2
+
+while True:
+    try:
+        delivery = int(input ("Please enter a number ≧ ◠ ᴥ ◠ ≦ ")) #int for integer input 
+        if delivery == 1:
+            print ("You have chosen to pickup your order （っ＾▿＾)っ♥") #prints this message if typed 2
+            break
+    
+        elif delivery == 2:
+            print ("You have chosen delivery for today ʕ•́ᴥ•̀ʔ") #prints this message if typed 1
+            break
+    
+    except ValueError: 
+        print ("Sorry, that was an invalid input ( > ⌂ < ) ") #prints this message if input entered incorrectly"
+        print ("Please enter 1 or 2")
