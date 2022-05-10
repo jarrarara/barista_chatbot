8378e792b058b1528a928cebad3fe5c099f2bfc9
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-10 19:53:55 +1200

    pickup and delivery added

---
+++ Components/pickup_delivery_menu_v1.py
@@ -0,0 +1,13 @@
+#user can choose whether they want pickup or delivery
+
+print ("Do you want your special drink to be picked up or delivered?")
+
+print ("For delivery, please enter 'd' or enter 'p' for pickup")
+
+delivery = input ()
+
+if delivery == "d":
+    print ("You have chosen delivery for today ʕ•́ᴥ•̀ʔ")
+    
+elif delivery == "p":
+    print ("You have chosen to pickup ypur order （っ＾▿＾)っ♥ ")
