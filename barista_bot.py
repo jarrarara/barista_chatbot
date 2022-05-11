12a68c54355632de06a3f4803466e723cf538a2a
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-11 20:43:37 +1200

    integrating pickup or delivery menu into main code

--- barista_bot.py
+++ barista_bot.py
@@ -7,6 +7,7 @@ from random import randint #random intiger (random number generator)
 names = ["Zunalora", "Kyzumath", "Malos", "Manazyri", "Zycandos", "Cornelius", "Aquilla", "Lelaine", "Doriath", "Zephyr"]
 
 #Welcome message with random name
+
 def welcome(): #defines fuction welcome
     '''
     Purpose: to generate a random name from the list and print a welcome message to the user
@@ -21,8 +22,32 @@ def welcome(): #defines fuction welcome
 
 #Menu for pickup or delivery
 
+def pickup(): 
+    print ("Do you want your magical drink to be picked up or delivered?")
 
+    print ("For pickup, please enter '1'")
+    print ("For delivery, please enter '2'")
 
+    low = 1 #repesents that lowest input is 1
+    high = 2 #represents that highest input is 2
+
+    while True:
+        try:
+            delivery = int(input ("Please enter a number ≧ ◠ ᴥ ◠ ≦  ")) #int for integer input 
+            if delivery >= 1 and delivery <= 2:
+                if delivery == 1:
+                    print ("You have chosen to pickup your order （っ＾▿＾)っ♥") #prints this message if typed 2
+                    break
+        
+                elif delivery == 2:
+                    print ("You have chosen delivery for today ʕ•́ᴥ•̀ʔ") #prints this message if typed 1
+                    break
+        
+            else:
+                print("Number must be '1' or '2'")
+        except ValueError: 
+            print ("Sorry, that was an invalid input ( > ⌂ < ) ") #prints this message if input entered incorrectly"
+            print ("Please enter 1 or 2")
 
 #Pickup information - name and phone number
 
@@ -72,4 +97,5 @@ def main():
     Returns: None
     '''
     welcome()
+    pickup()
 main()
