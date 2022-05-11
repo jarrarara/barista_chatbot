3b30e590cf2530757e7a44848c9a8061061ffcf1
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-11 22:20:49 +1200

    pickup info v3

--- Components/pickup_v3.py
+++ Components/pickup_v3.py
@@ -1,11 +1,13 @@
+customer_details = {}
+
 print("Please enter your pickup information (*＾▽＾)")
 
 #customer name
 valid = False
 while not valid: 
-    name = input("Please state your name on the space prodided: ")
-    if name != "":
-        print(name)
+    customer_details ["name"] = input("Please state your name on the space prodided: ")
+    if customer_details ["name"] != "":
+        print(customer_details ["name"])
         break #breaks the 'asking for name" cycle out of the loop
     else:
         print("Sorry, this cannot be left blank ( T ^ T )")
@@ -13,9 +15,9 @@ while not valid:
 #customer phone
 valid = False
 while not valid: 
-    phone = input("Please enter your phone number on the space provided: ")
-    if phone != "":
-        print(phone)
+    customer_details ["phone"] = input("Please enter your phone number on the space provided: ")
+    if customer_details ["phone"] != "":
+        print(customer_details ["phone"])
         break #breaks the 'asking for phone number" cycle out of the loop
     else:
         print("Sorry, this cannot be left blank ( T ^ T )")
