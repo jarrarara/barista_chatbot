8820fb0fd88f0c31ede8d652d1067f9f4835e6b4
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-16 16:25:28 +1200

    manu added

---
+++ Components/drinks_menuv1.py
@@ -0,0 +1,14 @@
+
+#menu that users can choose from and how many drinks they want
+drink_names = ['Heart of the City','Golden Eden','Night of Swirling Stars','Moonlit Alley','Foamy Reef','Scholar Afternoon',
+            'Brightcrown','Laughter and Cheer','Misty Garden','Love Poem','Sweet Cider Kiss','Dawning Dew','Gray Valley Sunset','Snow-Covered Kiss']
+
+drink_prices = ['6.50','6.50','6.50','6.50','7.50','7.50','7.50','7.50','8.00','8.00','8.00','8.00','9.00','9.00']
+
+num_drinks = 14
+
+#print ("How many drinks would you like to order o(〃＾▽＾〃)o? ")
+#num_drink = int(input())
+
+for count in range (num_drinks):
+    print(count, drink_names[count], drink_prices[count]) #prints the drinks names and the drink price together
