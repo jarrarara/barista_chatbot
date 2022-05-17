7e634d36d373c6788cab8b58a2ec1fe44a3ac1a3
jarrarara <99291195+jarrarara@users.noreply.github.com>
2022-05-17 20:57:41 +1200

    order print v1 & v2 added

--- Components/order_print_v1.py
+++ Components/order_print_v1.py
@@ -4,8 +4,13 @@ order_list = ['Brightcrown','Laughter and Cheer','Misty Garden','Love Poem']
 #list store drink prices
 order_cost = ['7.50','7.50','7.50','8.00']
 
-count = 0
+cust_details = {'name':'Viktor', 'phone':'09876542', 'house':'90', 'street':'Merryland', 'suburb':'Flatbush'}
 
+#print("\n", cust_details['name'], "\n", cust_details['phone'], "\n", cust_details['house'], "\n", cust_details['street'], "\n", cust_details['suburb'])
+
+print("\n Customer Name:{} \n Customer Phone:{} \n Customer House number:{} \n Customer Street Name:{} \n Customer Suburb:{}" .format( cust_details['name'], cust_details['phone'], cust_details['house'], cust_details['street'], cust_details['suburb']))
+
+count = 0
 for item in order_list:
     print("{} ${}" .format(item, order_cost[count]))
     count = count+1
